78aa0c782bb5655f38d566ef63fe555c076da3da
irosado21 <i_rosado@fpzornotza.com>
2022-01-13 10:29:54 +0100

    subida del header y footer dinamico total
    Co-authored-by: amoris123 <amoris123@users.noreply.github.com>

---
+++ resources/js/dinamico.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () =>{
+    loadHeaderFooter()
+})
+
+async function loadHeaderFooter(){
+    await new Promise (()=>{
+        var ruta = $(location).attr('href').split('/').pop().split('.')[0];
+
+        switch (ruta) {
+            case '':
+                $('#dynamicFooter').load('../resources/pages/footer.html');
+                $('#dynamicHeader').load('../resources/pages/header.html');
+            break;
+
+            case 'index':
+                $('#dynamicFooter').load('../resources/pages/footer.html');
+                $('#dynamicHeader').load('../resources/pages/header.html');
+            break;
+        
+            case 'paciente':
+                $('#dynamicFooter').load('footer.html');
+                $('#dynamicHeader').load('header.html');
+            break 
+        }
+    })
+}
+  
