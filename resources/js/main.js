d15d6ffb2846ac66e20ceb65812ded15f491e52a
irosado21 <i_rosado@fpzornotza.com>
2022-01-14 13:15:55 +0100

    cambios relevantes
    Co-authored-by: amoris123 <amoris123@users.noreply.github.com>

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,8 +2,16 @@ $('#inicio').submit(() => {
     var tis = $('#tis').val();
     var surname = $('#surname').val();
     var birth = $('#birth').val();
-
+console.log('eres bobo');
 
 
     return false;
 });
+
+$('#iniciarAdmin').click(() => {
+    // console.log($('#usuario').val());
+    // console.log($('#conta').val());
+    alert('estas dentro');
+    console.log(1);
+    return false;
+})
