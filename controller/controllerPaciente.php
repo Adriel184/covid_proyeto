f710d19333e6e90d2490245c71079a74bc586923
Azerzax <azerimrane@gmail.com>
2022-01-19 08:27:50 +0100

    comm

--- controller/controllerPaciente.php
+++ controller/controllerPaciente.php
@@ -1,30 +1,48 @@
 <?php
 
-    session_start();
-    session_unset();
-    session_destroy();
-
     include_once '../model/paciente_model.php';
     $data=json_decode(file_get_contents("php://input"),true);
+    session_start();
 
-    $response=array();
+    $accion=$data['accion'];
 
-    $tis="12345678";
-    $fecha_nac="2001-12-10";
-    $apellido="crespo";
+    if ($accion=="login") {
+    
+        $response=array();
+    
+        $tis=$data['tis'];
+        $fecha_nac=$data['fecha_nac'];
+        $apellido=$data['apellido'];
+    
+        $paciente=new paciente_model();
+        $paciente->setTis($tis);
+        $paciente->setFecha_nac($fecha_nac);
+        $paciente->setApellido($apellido);
+    
+        $response= $paciente->loginPaciente();
+    
+        if ($response['logged']) {
+            $_SESSION['tis']=$paciente->getTis();
+        }
+    
+        echo json_encode($response);
+        
+        unset($response);
+        
+    }elseif ($accion=="load") {
+        
+        $response=array();
+    
+        $tis= $_SESSION['tis'];
+    
+        $paciente=new paciente_model();
+        $paciente->setTis($tis);
+    
+        $response= $paciente->getPaciente();
+    
+        echo json_encode($response);
+        
+        unset($response);
 
-    $paciente=new paciente_model();
-    $paciente->setTis($tis);
-    $paciente->setFecha_nac($fecha_nac);
-    $paciente->setApellido($apellido);
-
-    $response= $paciente->loginPaciente();
-
-    if ($response['logged']) {
-        session_start();
-        $_SESSION['tis']=$paciente->getTis();
     }
 
-    echo json_encode($response);
-    
-    unset($response);
