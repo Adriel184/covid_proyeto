a5fc7f1a14d05617670026fe78988db6021e47fc
irosado21 <i_rosado@fpzornotza.com>
2022-01-20 13:18:28 +0100

    cambios visuales de modals y demas

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,5 @@
 $('#loginPaciente').click(() => {
     var x="paciente"
-    console.log("Se ha hecho click en el boton confirmar para entrar como: " +x);
     login(x);  
 });
 
@@ -18,32 +17,32 @@ function login(x) {
     var surname = $('#surname').val();
     var birth = $('#birth').val();
 
-    console.log("Se ha entrado en la funcion login para: --->" +x+ "  Datos:")
-    console.log(tis);
-    console.log(surname);
-    console.log(birth);
-
     var url = "controller/controllerLogin.php";
-    
-    if(x=="paciente"){
-        var data = { 'tis':tis, 'apellido':surname, 'fecha_nac':birth, "accion":x};
-    }else if (x=="admin") {
-        var data = {"accion":x};
-    }
-
-    fetch(url, {
-    method: 'POST', // or 'POST'
-    body: JSON.stringify(data), // data can be `string` or {object}!
-    headers:{'Content-Type': 'application/json'}  //input data
-    
-    })
-    .then(res => res.json()).then(result => {
-    
-        console.log( "El login ha sido correcto?: "+result.logged);
-
-        if (result.logged) {
-            window.location.href="resources/pages/paciente.html";
+    if ((tis != null && tis != "") && (surname != null && surname != "") && (birth != null && birth != "")) {
+        if(x=="paciente"){
+            var data = { 'tis':tis, 'apellido':surname, 'fecha_nac':birth, "accion":x};
+        }else if (x=="admin") {
+            var data = {"accion":x};
         }
-    }).catch(error => console.error('Error status:', error));
+
+        fetch(url, {
+        method: 'POST', // or 'POST'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{'Content-Type': 'application/json'}  //input data
+        
+        })
+        .then(res => res.json()).then(result => {
+        
+            console.log( "El login ha sido correcto?: "+result.logged);
+
+            if (result.logged) {
+                window.location.href="resources/pages/paciente.html";
+            }
+        }).catch(error => console.error('Error status:', error));
+    }else{
+        $('#mensajeError').show(150);
+        $('#loginPaciente').removeClass('my-3');
+    }
+        
 };
 
