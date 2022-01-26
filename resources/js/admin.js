bc0d9bb04256b4e12b6ce9445eadc4ff56e772ad
irosado21 <i_rosado@fpzornotza.com>
2022-01-26 13:32:39 +0100

    Buscar por tis hecho

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -15,19 +15,28 @@ $(document).ready(function() {
         $(centros).each(function(i) {
             if(centros[i].provincia.includes("Vizcaya")){
                 console.log(centros[i]);
-                vizcaya += "<button type='button' class='collapsible-no-back' data-bs-toggle='modal' data-bs-target='#dataCentro'>" + centros[i].nombre + "</button>";
+                vizcaya += "<button type='button' class='collapsible-no-back centroM' data-bs-toggle='modal' data-bs-target='#dataCentro' "
+                + "data-id='" + centros[i].id + "' "
+                + "data-nombre='" + centros[i].nombre + "' "
+                + ">" + centros[i].nombre + "</button>";
             }
             else if(centros[i].provincia.includes("Gipuzkoa")){
-                gipuzkoa += "<button type='button' class='collapsible-no-back' data-bs-toggle='modal' data-bs-target='#dataCentro'>" + centros[i].nombre + "</button>";
+                gipuzkoa += "<button type='button' class='collapsible-no-back centroM' data-bs-toggle='modal' data-bs-target='#dataCentro'>" + centros[i].nombre + "</button>";
             }
-            else if(centros[i].provincia.includes("Vizcaya")){
-                navarra += "<button type='button' class='collapsible-no-back' data-bs-toggle='modal' data-bs-target='#dataCentro'>" + centros[i].nombre + "</button>";
+            else if(centros[i].provincia.includes("Navarra")){
+                navarra += "<button type='button' class='collapsible-no-back centroM' data-bs-toggle='modal' data-bs-target='#dataCentro'>" + centros[i].nombre + "</button>";
             }
         });
         
         $('#vizcaya').append(vizcaya);
         $('#gipuzkoa').append(gipuzkoa);
         $('#navarra').append(navarra);
+
+        $('.centroM').click(function log() {
+            var myBookId = $(this).data();
+            console.log(myBookId);
+            //$(".modal-body #bookId").val(myBookId);
+        });
       }).catch(error => console.error('Error status:', error));
 });
 
@@ -41,10 +50,32 @@ $('#buscaTis').click(function () {
         body: JSON.stringify(data),
         headers:{'Content-Type': 'application/json'}  
     }).then(res => res.json()).then(result => {
-
+        
         console.log(result);
 
-        
+        var fechaPcr = result.paciente.fecha_pcr_pstv;
+
+        if (fechaPcr == null) {
+            fechaPcr = 'NO TIENE FECHA';
+        }
+
+        $('#infoPaciente').html(
+            "<div class='col-7'>" +
+              "<label class='fw-bold'>Nombre del paciente:</label><span> "+result.paciente.nombre+"</span><br>" +
+              "<label class='fw-bold'>Apellido del paciente: </label><span> "+result.paciente.apellido+"</span><br>" +
+              "<label class='fw-bold'>fecha de nacimiento del paciente: </label><span> "+result.paciente.fecha_nac+"</span><br>" +
+              "<label class='fw-bold'>Nombre del centro asignado del paciente: </label><span> "+result.paciente.centro.nombre+"</span><br>" +
+              "<label class='fw-bold'>Poblacion del centro asignado del paciente: </label><span> "+result.paciente.centro.poblacion+"</span><br>" +
+              "<label class='fw-bold'>Provincia del centro asignado del paciente: </label><span> "+result.paciente.centro.provincia+"</span><br>" +
+            "</div>" +
+            "<div class='col-5'>" +
+              "<label class='fw-bold'>Ultima pcr positiva del paciente: </label><span> "+fechaPcr+"</span><br>" +
+              "<label class='fw-bold'>Nº de dosis del paciente: </label><span> "+result.paciente.citas.dosis+"</span><br>" +
+              "<label class='fw-bold'>Id de la cita del paciente: </label><span> "+result.paciente.citas.id+"</span><br>" +
+              "<label class='fw-bold'>Fecha de la cita del paciente: </label><span> "+result.paciente.citas.fecha.split(" ")[0]+"</span><br>" +
+              "<label class='fw-bold'>Hora de la cita del paciente: </label><span> "+result.paciente.citas.fecha.split(" ")[1]+" </span><br>" +
+            "</div>"
+        );
 
     }).catch(error => console.error('Error status:', error));
 
